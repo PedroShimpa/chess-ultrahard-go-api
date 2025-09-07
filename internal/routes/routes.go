96d11949fd33e8583fe7e7572e76b4d4b1d33d9d
Pedro do PHP <pedrofalck2000@gmail.com>
2025-09-07 00:52:25 -0300

    Update routes.go

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,16 +1,28 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/pedroShimpa/go-api/internal/controllers"
 	"github.com/pedroShimpa/go-api/internal/middleware"
 	"github.com/pedroShimpa/go-api/internal/repositories"
 	"github.com/pedroShimpa/go-api/internal/services"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // ou "*" para todos
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	userRepo := &repositories.UserRepository{DB: db}
 	authService := &services.AuthService{UserRepo: userRepo}
 	userController := &controllers.UserController{AuthService: authService}
@@ -28,6 +40,5 @@ func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
 			protected.POST("/game/new", controllers.StartGame)
 			protected.POST("/game/move", controllers.MakeMove)
 		}
-
 	}
 }
